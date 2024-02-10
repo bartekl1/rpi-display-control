d0704551026e9106ca869fe085795c6737da9979
bartekl1 <123882445+bartekl1@users.noreply.github.com>
2024-02-10 17:58:49 +0100

    First version

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask
+
+import json
+import os
+
+app = Flask(__name__)
+
+
+@app.route("/api/on", methods=["POST"])
+def turn_on_screen():
+    os.system("xset dpms force on")
+    return {"status": "ok"}
+
+
+@app.route("/api/off", methods=["POST"])
+def turn_off_screen():
+    os.system("sleep 1 && xset dpms force off")
+    return {"status": "ok"}
+
+
+if __name__ == "__main__":
+    with open("configs.json") as file:
+        configs = json.load(file)
+
+    host = configs["host"] if "host" in configs.keys() else None
+    port = configs["port"] if "port" in configs.keys() else None
+    debug = configs["debug"] if "debug" in configs.keys() else None
+
+    app.run(host=host, port=port, debug=debug)
